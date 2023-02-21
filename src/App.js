c82bf8191f74790f5dd7df288ee20c90e1d3fbba
Arfanur Rahman <arfanur36@gmail.com>
2023-02-21 15:44:27 +0600

    79-2 LoadingButton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { LoadingButton } from "@mui/lab";
+import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
+// import Button from '@mui/material/Button';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [submitting, set_submitting] = useState(false);
+
+	useEffect(() => {
+		const id = setTimeout(() => {
+			set_submitting(false);
+		}, 1000);
+
+		return () => {
+			clearTimeout(id);
+		};
+	}, [submitting]);
+
+	return (
+		<div className="app">
+			<div
+				style={{
+					background: "#f7f7f7",
+					width: "400px",
+					height: "400px",
+					borderRadius: "1rem",
+					padding: "2rem",
+				}}
+			>
+				<Button
+					variant="contained"
+					disableRipple={false}
+					color="secondary"
+					fullWidth
+					sx={{ mb: 2 }}
+				>
+					Checkout
+				</Button>
+
+				<LoadingButton
+					loading={submitting}
+					// loadingIndicator="Loading…"
+					variant="outlined"
+					onClick={() => set_submitting(true)}
+				>
+					Fetch data
+				</LoadingButton>
+			</div>
+		</div>
+	);
 }
 
 export default App;
