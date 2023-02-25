ac7933c59b04e3d8a11b316b696d4b9dfb7d0a94
Arfanur Rahman <arfanur36@gmail.com>
2023-02-25 16:34:27 +0600

    79-7 Styled API and Project Part - 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./component/Layout/Layout";
-import Home from "./pages/Home/Home";
+import Home from "./pages/Home/Home/Home";
 import { theme } from "./theme/theme";
 
 const router = createBrowserRouter([
