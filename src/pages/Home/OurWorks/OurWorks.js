c8b142b87c845af9b244d8e62dcf2393952cc3e0
arfan36 <arfanur36@gmail.com>
2023-04-20 14:32:29 +0600

    79-10 Project Part - 5 (Our works heading)

--- src/pages/Home/OurWorks/OurWorks.js
+++ src/pages/Home/OurWorks/OurWorks.js
@@ -1,18 +1,20 @@
-import { Box } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
 import React from "react";
 import SectionTitle from "../../../component/SectionTitle/SectionTitle";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const OurWorks = () => {
 	const [value, setValue] = React.useState(0);
 
-	const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+	const handleChange = (event, newValue) => {
+		console.log(newValue);
 		setValue(newValue);
 	};
 
 	return (
-		<Box sx={{ mt: 5 }}>
+		<Box sx={{ my: 5, mb: 10 }}>
 			{/* heading section */}
 			<Box
 				sx={{
@@ -28,16 +30,61 @@ const OurWorks = () => {
 						textAlign: "left",
 					}}
 				/>
-				<Box>
+				<Box
+					sx={{
+						display: "flex",
+						justifyContent: "space-between",
+						alignItems: "center",
+						gap: 2,
+					}}
+				>
 					<Tabs
 						value={value}
 						onChange={handleChange}
 						centered
+						sx={{
+							"& .MuiTabs-indicator": {
+								display: "none",
+							},
+
+							"& .Mui-selected": {
+								fontWeight: "bold",
+								color: "primary.green",
+							},
+							"& .MuiButtonBase-root": {
+								textTransform: "capitalize",
+							},
+						}}
 					>
-						<Tab label="Item One" />
-						<Tab label="Item Two" />
-						<Tab label="Item Three" />
+						<Tab label="All" />
+						<Tab label="Web Design" />
+						<Tab label="Mobile App" />
 					</Tabs>
+					<Box>
+						<IconButton
+							sx={{
+								border: "1px solid #959ead",
+								mr: 2,
+							}}
+							onClick={() => setValue(value - 1)}
+							disabled={value === 0}
+						>
+							<ArrowBackIcon />
+						</IconButton>
+						<IconButton
+							sx={{
+								border: "1px solid #959ead",
+							}}
+							onClick={() => setValue(value + 1)}
+							disabled={value === 2}
+						>
+							<ArrowBackIcon
+								sx={{
+									transform: "rotate(180deg)",
+								}}
+							/>
+						</IconButton>
+					</Box>
 				</Box>
 			</Box>
 		</Box>
